b379d25e20915ec6da851acdb2c45e00e1d4200a
namikkaya <namikkaya86@gmail.com>
2020-03-07 17:59:41 +0300

    Initial Commit

---
+++ Tabbar_tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tabbar_tutorial
+//
+//  Created by namik kaya on 7.03.2020.
+//  Copyright © 2020 namikkaya86@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
